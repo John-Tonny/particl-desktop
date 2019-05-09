fde2cb6abe687ddb39e4ad1cbe1d262aebc0add6
John-Tonny <szlhtao@tom.com>
2019-05-09 10:21:31 +0800

    0509-update2

--- main.js
+++ main.js
@@ -103,6 +103,7 @@ function initMainWindow() {
     },
   });
 
+  mainWindow.webContents.openDevTools();
   // Hide the menu bar, press ALT
   // to show it again.
   mainWindow.setMenuBarVisibility(false);
